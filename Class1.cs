51978cfa4d28803e543b63857b1a29c73f3d1266
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 10:50:59 +0530

    Exceptionhandling

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ExceptionHandling
+{
+    class Class1
+    {
+    }
+    [Serializable]
+    class StudentNotFoundException : Exception
+    {
+        public string StudentName { get; set; }
+        public StudentNotFoundException() { }
+
+        public StudentNotFoundException(string message) : base(message)  {}
+
+    //Console.WriteLine("alling base class message displayed here");
+        public StudentNotFoundException(string Message, Exception InnerException) : base(Message, InnerException) {}
+
+        public StudentNotFoundException(string message, string studentName)
+    : this(message)
+        {
+            StudentName = StudentName;
+        }
+        
+ 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demo_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                //Console.WriteLine("Enter your fav num");
+                //string No = Console.ReadLine();
+                //INT32.Parse(No);
+
+                //Console.WriteLine("No you entered {0}. valid integer 32",No);
+
+                throw new StudentNotFoundException("student not found", "V Vaishu");
+            }
+            catch(StudentNotFoundException s)
+            {
+                Console.WriteLine("student not found exception"+s.StudentName);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("invalid integer",fe.Source);
+                throw;
+
+             
+            }
+            catch (OverflowException)
+            {
+                OverflowException a = new OverflowException();
+                Console.WriteLine("value you entered is too big");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("some global exception has occured we are working on it..");
+            }
+            finally
+            {
+                Console.WriteLine("Hope this object oriented");
+            }
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
